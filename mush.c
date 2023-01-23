84f89839d5872ad940631fd57397efee16584558
Julian Canepa <julian.l.canepa@gmail.com>
2023-01-23 09:55:46 -0800

    Boilerplate for shell program.

---
+++ mush.c
@@ -0,0 +1,11 @@
+#include <stdio.h>    // printf, fgets, perror
+#include <stdlib.h>   // exit
+#include <string.h>   // strtok, strcmp
+#include <unistd.h>   // fork, execvp
+#include <sys/wait.h> // wait
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
