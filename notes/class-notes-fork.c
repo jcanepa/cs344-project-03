4e1d3dc5314772b7e1378fa81168b7b9ec4ffa1f
Julian Canepa <julian.l.canepa@gmail.com>
2023-01-23 11:35:29 -0800

    Add fork, exec example.

--- notes/class-notes-fork.c
+++ notes/class-notes-fork.c
@@ -1,12 +1,41 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <sys/wait.h>
+#include <stdlib.h>
 
 int main(void)
 {
     printf("Before fork\n");
 
     // makes a new process that's a copy of the current process
-    fork();
+    pid_t pid = fork();
 
+    // if we run this again, both parent and child will create a child (4 total)
+    // fork();
+
+    printf("Fork returned %d\n", pid);
+
+    if (pid == 0)
+    {
+        // child
+        printf("Child PID is %d\n", getpid());
+        printf("Child parent PID is %d\n", getppid());
+
+        char *new_argv[] = {"ls", NULL};
+
+        execvp("ls", new_argv);
+
+        perror("exec");
+        exit(0);
+    }
+
+    // parent
+    wait(NULL); // use null when we don't care about the exit status
+    // os wakes parent when one of child processes dies
+
+    printf("Parent PID is %d\n", getpid());
+    printf("Parent child PID is %d\n", pid);
+
+    // this is run 2^ (times fork was run)
     printf("After fork\n");
 }
