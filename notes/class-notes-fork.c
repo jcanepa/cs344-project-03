12083e5a52d6bab706850e581ffcb6e391736bbb
Julian Canepa <julian.l.canepa@gmail.com>
2023-01-23 11:37:57 -0800

    Simplify fork exec example with prints commented out.

--- notes/class-notes-fork.c
+++ notes/class-notes-fork.c
@@ -5,7 +5,7 @@
 
 int main(void)
 {
-    printf("Before fork\n");
+    printf("Before fork...\n");
 
     // makes a new process that's a copy of the current process
     pid_t pid = fork();
@@ -13,13 +13,13 @@ int main(void)
     // if we run this again, both parent and child will create a child (4 total)
     // fork();
 
-    printf("Fork returned %d\n", pid);
+    // printf("Fork returned %d\n", pid);
 
     if (pid == 0)
     {
         // child
-        printf("Child PID is %d\n", getpid());
-        printf("Child parent PID is %d\n", getppid());
+        // printf("Child PID is %d\n", getpid());
+        // printf("Child parent PID is %d\n", getppid());
 
         char *new_argv[] = {"ls", NULL};
 
@@ -33,9 +33,9 @@ int main(void)
     wait(NULL); // use null when we don't care about the exit status
     // os wakes parent when one of child processes dies
 
-    printf("Parent PID is %d\n", getpid());
-    printf("Parent child PID is %d\n", pid);
+    // printf("Parent PID is %d\n", getpid());
+    // printf("Parent child PID is %d\n", pid);
 
     // this is run 2^ (times fork was run)
-    printf("After fork\n");
+    printf("All done!\n");
 }
