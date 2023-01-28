160c9275116c378e417b258d5b08608555a62990
Julian Canepa <julian.l.canepa@gmail.com>
2023-01-28 13:42:02 -0800

    Progressing on mush shell, replacing number datatypes.

--- mush.c
+++ mush.c
@@ -9,10 +9,18 @@ void print_prompt(void)
     printf("$ ");
 }
 
-int main(int argc, char const *argv[])
+int main(void)
 {
+    // until the user quits (control c)
     while (1)
     {
+        // 1. print a prompt
+        // 2. read a line fgets()
+        // 3. parse down to individual words strtok()
+        // 4. put words into char* aray
+        // 5. terminate array with NULL
+        // 6. call execvp() on words array
+
         print_prompt();
 
         /**
@@ -22,17 +30,20 @@ int main(int argc, char const *argv[])
         fgets(str, 100, stdin);
         printf("🦜 %s\n", str);
 
+        // end execution if user inputs `exit`
+        if (strcmp(str, "exit\n") == 0)
+        {
+            break;
+        }
+
         /**
-         * Separate str into individual words
+         * Separate command into individual words
          */
         char delimiter[2] = " ";
         char *token = strtok(str, delimiter);
 
-        /**
-         * Add words to an array
-         */
         int i = 0;
-        const char *command_words[5];
+        char *command_words[2];
 
         while (token != NULL)
         {
@@ -41,10 +52,26 @@ int main(int argc, char const *argv[])
             printf("Command parts are: %s\n", token);
             token = strtok(NULL, delimiter);
         }
-        // terminate the array
-        command_words[i] = "\0";
+        // terminate array
+        command_words[i] = NULL;
 
-        // Next: make it into a command...
+        for (unsigned long i = 0; i < sizeof(command_words); i++)
+            printf("%s ", command_words[i]);
+
+        pid_t pid = fork();
+
+        if (pid == 0)
+        {
+            // child process
+            printf("Child process running: %s\n", command_words[0]);
+
+            execvp("foo", command_words);
+            perror("exec");
+            exit(0); // success!
+        }
+
+        wait(NULL);
+        printf("done!\n");
     }
     return 0;
 }
