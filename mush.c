bd57050019b239ad659f46198721c475122ceab5
Julian Canepa <julian.l.canepa@gmail.com>
2023-01-28 17:33:30 -0800

    Continue to develop shell, implement cd commands handler.

--- mush.c
+++ mush.c
@@ -19,8 +19,8 @@ int main(void)
         fgets(str, 2048, stdin);
         // printf("🦜 %s\n", str);
 
-        if (strcmp(str, "exit\n") == 0) // end on `exit`
-            break;
+        if (strcmp(str, "exit\n") == 0) // end shell on `exit`
+            exit(0);
 
         /**
          * Separate command into words
@@ -37,6 +37,9 @@ int main(void)
             token = strtok(NULL, delimiter);
             i++;
         }
+        // delete new line char on last word
+        strtok(command_words[i - 1], "\n");
+
         command_words[i] = "/usr/bin";
         command_words[i + 1] = NULL;
 
@@ -49,6 +52,19 @@ int main(void)
         //     printf("%s\n", command_words[i]);
 
         /**
+         * Handle `cd`
+         */
+        int result;
+
+        if (strcmp(command_words[0], "cd\n") == 0)
+        {
+            result = chdir(command_words[1]);
+
+            if (result == -1)
+                perror("chdir");
+        }
+
+        /**
          * Create a child process to run the command
          */
         pid_t process_id = fork();
@@ -59,9 +75,18 @@ int main(void)
             //     "Child process to run %s\n",
             //     command_words[0]);
 
-            execvp("ls", command_words);
+            execvp(command_words[0], command_words);
+
+            // char *new_argv[] = {
+            //     "ls",
+            //     "-a",
+            //     "-h",
+            //     "/Users/jcanepa/code/cs344-project-03",
+            //     NULL};
+            // execvp("ls", new_argv);
+
             perror("exec");
-            exit(0); // success!
+            exit(1); // failure
         }
 
         wait(NULL);
